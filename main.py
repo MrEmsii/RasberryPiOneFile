177327739542d4de8818e0fa9661d8917350fad7
Patryk Szczepanik <skowik9@gmail.com>
2024-06-03 19:45:38 +0200

    global settings update

--- main.py
+++ main.py
@@ -40,6 +40,11 @@ class thread:
         t_ts.start()
         t_ts.join()
 
+    def Temp_Global():
+        t_tg = threading.Thread(target=operation.Temp_Global)
+        t_tg.name = inspect.stack()[0][3]
+        t_tg.start()
+
     def localization_thread():
         t_local = threading.Thread(target=operation.localization)
         t_local.name = inspect.stack()[0][3]
@@ -85,6 +90,11 @@ class operation:
         Temperature_Calculation.save(path)
         control.name_thread_start((threading.current_thread().getName()), threading.get_native_id())
 
+    def Temp_Global():
+        global temperature_list
+        temperature_list = Temperature_Calculation.tempALL()
+        control.name_thread_start((threading.current_thread().getName()), threading.get_native_id())
+
     def localization():
         data = operation.Weather_City()
         ConfigControl.edit_Config(path,[("city", data[0]),("IP_query", data[1])])
@@ -113,6 +123,56 @@ class operation:
         ConfigControl.edit_Config(path,[("IP_home",cmd)])
         control.name_thread_start((threading.current_thread().getName()), threading.get_native_id())
 
+class lcd_class:
+    @Another.save_error_to_file("log_bledow.txt")
+
+    def time(wait):
+        MyLCD.lcd_display_string_pos("Data: " + str(datetime.date.today()), 3, 2)
+        for i in range(int(wait/0.2)):
+            clock = datetime.datetime.now()
+            MyLCD.lcd_display_string_pos("Time: " + clock.strftime("%H:%M:%S"), 2, 2)
+            time.sleep(0.2)
+
+    def weather():
+        city = ConfigControl.collect_Config(path,"city")
+        temp_outside = ConfigControl.collect_Config(path,"temp_outside")
+        current_p_h = ConfigControl.collect_Config(path,"current_humidity") + " " + ConfigControl.collect_Config(path,"current_pressure")
+        info_weather = ConfigControl.collect_Config(path,"info_weather")
+        MyLCD.lcd_display_string_pos(str(city), 1, int((20 - len(str(city)))/2))
+        MyLCD.lcd_display_string_pos(str(temp_outside), 3, int((20 - len(str(temp_outside)))/2))
+        MyLCD.lcd_display_string_pos(str(current_p_h), 4, int((20 - len(str(current_p_h)))/2))
+
+        info_weather = " "*10 + info_weather + " "*10
+        for i in range (0, len(info_weather) - 20):
+            lcd_text = info_weather[i:(i+20)]
+            MyLCD.lcd_display_string(lcd_text, 2)
+            time.sleep(0.3)
+       
+    def temperatura(wait):
+        MyLCD.lcd_display_string_pos("Temperature:", 1, 4)
+        Room_temp = " Room = " + str(temperature_list[0]) + "\u00dfC "
+        OutDoor_temp = " OutDoor = " + str(temperature_list[1]) + "\u00dfC "
+        MyLCD.lcd_display_string_pos(str(Room_temp), 2, 4)
+        MyLCD.lcd_display_string_pos(str(OutDoor_temp), 3, 1)
+
+        for i in range(int(wait)):
+            RaspPI_temp = " RaspPI = " + str(CPUTemperature().temperature)[0:4] + "\u00dfC "
+            MyLCD.lcd_display_string_pos(str(RaspPI_temp), 4, 2)
+            time.sleep(0.7)
+
+    def pc_stats(wait):
+        description =  "CPU  " + "RAM " + "DISK "
+        MyLCD.lcd_display_string_pos(description, 1, 3)
+        ip_home = ConfigControl.collect_Config(path,"IP_home")
+        ip_query = ConfigControl.collect_Config(path,"IP_query")
+        MyLCD.lcd_display_string_pos(ip_query, 3, int((20 - len(ip_query))/2) - 1)
+        MyLCD.lcd_display_string_pos(ip_home, 4, int((20 - len(ip_home))/2) - 1)
+        for i in range(int(wait*4)):
+            mesh = str(str(psutil.cpu_percent(interval = 0.2)) + "% " + str(psutil.virtual_memory().percent) + "% " + str(psutil.disk_usage('/').percent) + "%")
+            MyLCD.lcd_display_string_pos(mesh, 2, 2)
+            time.sleep(.3)
+        
+
 class control:
     @Another.save_error_to_file("log_bledow.txt")
 
@@ -139,61 +199,21 @@ class control:
 
     def LCD_Control(time_stop_LCD, wait):
         control.name_thread_start((threading.current_thread().getName()), threading.get_native_id())
+        i = 0
         while datetime.datetime.now() < time_stop_LCD:
-            MyLCD.lcd_display_string_pos("Data: " + str(datetime.date.today()), 3, 2)
-            for i in range(int(wait/0.2)):
-                clock = datetime.datetime.now()
-                MyLCD.lcd_display_string_pos("Time: " + clock.strftime("%H:%M:%S"), 2, 2)
-                time.sleep(0.2)
+            if i % 2 == 0:
+                thread.Temp_Global()
+                lcd_class.time(wait)
+            else:
+                if i % 6 == 1:
+                    lcd_class.temperatura(wait)
+                elif i % 6 == 3:
+                    lcd_class.weather()
+                elif i % 6 == 5:
+                    lcd_class.pc_stats(wait)
+
+            i += 1
             MyLCD.lcd_clear()
-
-
-            city = ConfigControl.collect_Config(path,"city")
-            temp_outside = ConfigControl.collect_Config(path,"temp_outside")
-            current_p_h = ConfigControl.collect_Config(path,"current_humidity") + " " + ConfigControl.collect_Config(path,"current_pressure")
-            info_weather = ConfigControl.collect_Config(path,"info_weather")
-            MyLCD.lcd_display_string_pos(str(city), 1, int((20 - len(str(city)))/2))
-            MyLCD.lcd_display_string_pos(str(temp_outside), 3, int((20 - len(str(temp_outside)))/2))
-            MyLCD.lcd_display_string_pos(str(current_p_h), 4, int((20 - len(str(current_p_h)))/2))
-
-            info_weather = " "*5 + info_weather + " "*20
-            for i in range (0, len(info_weather) - 35):
-                lcd_text = info_weather[i:(i+20)]
-                MyLCD.lcd_display_string(lcd_text,2)
-                time.sleep(0.4)
-                
-
-
-# do some other stuff in the main process
-
-            temp_list = Temperature_Calculation.tempALL()
-
-            MyLCD.lcd_clear()
-
-            MyLCD.lcd_display_string_pos("Temperature:", 1, 4)
-            Room_temp = " Room = " + str(temp_list[0]) + "\u00dfC "
-            OutDoor_temp = " OutDoor = " + str(temp_list[1]) + "\u00dfC "
-            MyLCD.lcd_display_string_pos(str(Room_temp), 2, 4)
-            MyLCD.lcd_display_string_pos(str(OutDoor_temp), 3, 1)
-
-            for i in range(int(wait)):
-                RaspPI_temp = " RaspPI = " + str(CPUTemperature().temperature)[0:4] + "\u00dfC "
-                MyLCD.lcd_display_string_pos(str(RaspPI_temp), 4, 2)
-                time.sleep(0.7)
-            MyLCD.lcd_clear()
-
-            description =  "CPU  " + "RAM " + "DISK "
-            MyLCD.lcd_display_string_pos(description, 1, 3)
-            ip_home = ConfigControl.collect_Config(path,"IP_home")
-            ip_query = ConfigControl.collect_Config(path,"IP_query")
-            MyLCD.lcd_display_string_pos(ip_query, 3, int((20 - len(ip_query))/2) - 1)
-            MyLCD.lcd_display_string_pos(ip_home, 4, int((20 - len(ip_home))/2) - 1)
-            for i in range(int(wait*4)):
-                mesh = str(str(psutil.cpu_percent(interval = 0.2)) + "% " + str(psutil.virtual_memory().percent) + "% " + str(psutil.disk_usage('/').percent) + "%")
-                MyLCD.lcd_display_string_pos(mesh, 2, 2)
-                time.sleep(.3)
-            MyLCD.lcd_clear()
-
         MyLCD.backlight(0)
 
     @Another.save_error_to_file("error_log.txt")
@@ -245,10 +265,13 @@ class control:
                 MyLCD.lcd_clear()
                 control.kill_process("Emsii_LCD")
 
-            time.sleep(5)
+            time.sleep(0.3)
 
 def startingProces():
     MyLCD.backlight(0)
+    if os.path.isfile(path + "error_log.txt") == True:
+        os.remove(path + "error_log.txt")
+        
     if os.path.isfile(path + "Heat.db") == False:
         columns = ("data", "godzina", "temp_dot", "temp_comma", "jednostka")
         thread.Table_Maker_thread(path, "Heat.db", columns, "temperatura") 
@@ -267,7 +290,7 @@ def startingProces():
             "IP_query": "",
             "color": "",
             "brightness": 0.5,
-            "effect": 0,
+            "effects": 0,
             "leds_speed": 1,
             "hour_start_LCD": 8,
             "hour_stop_LCD": 23,
